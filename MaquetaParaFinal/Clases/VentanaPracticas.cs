b47068e5130bf85c819c778ac2783b352888526f
hilet <hilet@A04M09>
2023-10-10 10:39:18 -0300

    Agregar archivos de proyecto.

---
+++ MaquetaParaFinal/Clases/VentanaPracticas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MaquetaParaFinal.View
+{
+    public partial class Practicas : Page
+    {
+        public event EventHandler VolverClicked;
+        private void VolverMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Principal.Visibility = Visibility.Collapsed;
+            VolverClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
