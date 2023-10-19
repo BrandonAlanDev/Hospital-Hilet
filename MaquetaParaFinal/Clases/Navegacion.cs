ef91a9616867235bab1a0bef92fe14207ed7cc6a
BrandonAlanDev <brandoncarabajal@gmail.com>
2023-10-18 22:57:16 -0300

    Vista y Navegacion

---
+++ MaquetaParaFinal/Clases/Navegacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaquetaParaFinal
+{
+    public partial class MainWindow : Window
+    {
+        private void LoadHome(object sender, RoutedEventArgs e)
+        {
+            FrameNavegacion.NavigationService.Navigate(new Uri("View/Index.xaml", UriKind.Relative));
+        }
+        private void LoadIngresos(object sender, RoutedEventArgs e)
+        {
+            FrameNavegacion.NavigationService.Navigate(new Uri("View/Index.xaml", UriKind.Relative));
+        }
+        private void LoadPacientes(object sender, RoutedEventArgs e)
+        {
+            FrameNavegacion.NavigationService.Navigate(new Uri("View/Pacientes.xaml", UriKind.Relative));
+        }
+        private void LoadPracticas(object sender, RoutedEventArgs e)
+        {
+            FrameNavegacion.NavigationService.Navigate(new Uri("View/Practicas.xaml", UriKind.Relative));
+        }
+        private void LoadMedicos(object sender, RoutedEventArgs e)
+        {
+            FrameNavegacion.NavigationService.Navigate(new Uri("View/Medicos.xaml", UriKind.Relative));
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- MaquetaParaFinal/View/Index.xaml.cs
+++ MaquetaParaFinal/View/Index.xaml.cs
@@ -13,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MaquetaParaFinal
+namespace MaquetaParaFinal.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Index.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Index : Page
     {
-        public MainWindow()
+        public Index()
         {
             InitializeComponent();
-            FrameNavegacion.NavigationService.Navigate(new Uri("View/Index.xaml", UriKind.Relative));
         }
     }
 }
