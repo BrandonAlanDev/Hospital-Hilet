483486a13fde591f7bc79d2cef638a832049bb61
Luchi <lucianoceles@gmail.com>
2023-10-24 17:41:39 -0300

    Ventana Ingresos Creada

---
+++ MaquetaParaFinal/Clases/VentanaIngresos.cs
@@ -0,0 +1,20 @@
+﻿using MaquetaParaFinal.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MaquetaParaFinal.View
+{
+    public partial class Ingresos : Page
+    {
+        Conectar conectar = new Conectar();
+        private void DataGridIngresos_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGridIngresos.ItemsSource = conectar.Des
+        }
+    }
+}
