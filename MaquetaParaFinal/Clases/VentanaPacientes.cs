c8260ebe722e15c2e22687dcdd5ecd9f0f254883
Luchi <lucianoceles@gmail.com>
2023-10-24 19:49:17 -0300

    Metodo Selecionar ON

--- MaquetaParaFinal/Clases/VentanaPacientes.cs
+++ MaquetaParaFinal/Clases/VentanaPacientes.cs
@@ -1,6 +1,7 @@
 ﻿using MaquetaParaFinal.Clases;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace MaquetaParaFinal.View
     public partial class Pacientes : Page 
     {
         Conectar conectar = new Conectar();
-
+        private int NumeroFilaId = 0;
         private readonly Dictionary<string, string> Dicpacientes = new Dictionary<string, string> //Seria la forma de hacerlo una const, con el readonly.
         {
             { "Nombre", "txtNombre" }, //txtNombre es el nombre del textbox.
@@ -48,11 +49,6 @@ namespace MaquetaParaFinal.View
                 }
             }
         }
-        private void DataGridPacientes_Loaded(object sender, RoutedEventArgs e)
-        {
-            DataGridPacientes.ItemsSource = conectar.DescargaTablaPaciente().DefaultView;
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             TxtBoxes.IsEnabled = true;
@@ -67,6 +63,37 @@ namespace MaquetaParaFinal.View
         {
             //GARGAR LOCALIDAD Y EXTRAER LA FK
             // conectar.AgregarPaciente(txtNombre,txtApellido,txtFecha_De_Nacimiento,txtDni,txtEmail,txtTelefono,txtCalle,txtNro,txtPiso,"SinHacer");
+        }
+        private void CargarSeleccion() 
+        {
+            if (DataGridPacientes.SelectedItem != null && DataGridPacientes.Items.Count >= NumeroFilaId)
+            {
+                DataGridPacientes.SelectedIndex = NumeroFilaId;
+                DataRowView row = (DataRowView)DataGridPacientes.SelectedItem;
+                txtNombre.Text = row["Nombre"].ToString();
+                txtApellido.Text = row["Apellido"].ToString();
+                txtDni.Text = row["Dni"].ToString();
+                txtEmail.Text = row["Email"].ToString();
+                txtFecha_De_Nacimiento.Text = row["Fecha De Nacimiento"].ToString();
+                txtTelefono.Text = row["Telefono"].ToString();
+                txtCalle.Text = row["Calle"].ToString();
+                txtNro.Text = row["Numero"].ToString();
+                txtLocalidad.Text = row["Localidad"].ToString();
+                txtCodPostas.Text = row["Codigo Postal"].ToString();
+                txtPiso.Text = row["Piso"].ToString();
+            }
+        }
+        private void DataGridPacientes_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGridPacientes.ItemsSource = conectar.DescargaTablaPaciente().DefaultView;
+        }
+
+
+        private void DataGridPacientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataRowView row = (DataRowView)DataGridPacientes.SelectedItem;
+            NumeroFilaId = int.Parse(row["ID"].ToString())-1; //-1 Porque el Datagrid comienza en 0 y el id en 1
+            CargarSeleccion();
         }
     }
 }
