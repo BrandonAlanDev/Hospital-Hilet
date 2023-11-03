3dc16d07c2b546b8d42a81f3469af651fab9e987
LucasBeriVerst <lucasberitich@gmail.com>
2023-11-03 11:27:13 -0300

    .

--- MaquetaParaFinal/Clases/Agregar/VentanaAgregarPractica.cs
+++ MaquetaParaFinal/Clases/Agregar/VentanaAgregarPractica.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Input;
+using System.Data;
+using MaquetaParaFinal.Clases;
 
 namespace MaquetaParaFinal.View.Agregar
 {
     public partial class AgregarPractica : Window
     {
+        Conectar conectar = new Conectar();
+
         private void Principal_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (Mouse.LeftButton == MouseButtonState.Pressed)
@@ -18,6 +22,7 @@ namespace MaquetaParaFinal.View.Agregar
                 DragMove();
             }
         }
+
         private void txtNombrePractica_GotFocus(object sender, RoutedEventArgs e)
         {
             if (txtNombrePractica.Text == "Nombre")
@@ -33,11 +38,17 @@ namespace MaquetaParaFinal.View.Agregar
                 txtNombrePractica.Text = "Nombre";
             }
         }
+
         private void btnCancelar_Click(object sender, RoutedEventArgs e) => this.Close();
 
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Border_Loaded(object sender, RoutedEventArgs e)
+        {
+            //DataTable dtespecialidad = 
         }
     }
 }
