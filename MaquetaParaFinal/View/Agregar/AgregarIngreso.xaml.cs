0e820e6d7737ca1f9822ec738b8b55090f33aa6f
Ale <alejandroverteche@gmail.com>
2023-11-06 11:10:36 -0300

    Ingresos

---
+++ MaquetaParaFinal/View/Agregar/AgregarIngreso.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaquetaParaFinal.View.Agregar
+{
+    /// <summary>
+    /// Lógica de interacción para AgregarIngreso.xaml
+    /// </summary>
+    public partial class AgregarIngreso : Page
+    {
+        public AgregarIngreso()
+        {
+            InitializeComponent();
+        }
+    }
+}
