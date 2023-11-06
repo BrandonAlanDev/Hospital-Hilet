eb6bbaf8d6a33eeff4645da7e2e907dff852022d
Ale <alejandroverteche@gmail.com>
2023-11-06 11:43:19 -0300

    Cositas

--- MaquetaParaFinal/View/Agregar/AgregarIngreso.xaml.cs
+++ MaquetaParaFinal/View/Agregar/AgregarIngreso.xaml.cs
@@ -28,7 +28,23 @@ namespace MaquetaParaFinal.View.Agregar
         private void txtMedico_Loaded(object sender, RoutedEventArgs e)
         {
             DataTable dt = conectar.DescargaTablaProfesinales();
+            List<string> data = new List<string>();
 
+            foreach (DataRow row in dt.Rows)
+            {
+                data.Add(row["Apellido"].ToString()+ row[" "].ToString()+ row["Nombre"].ToString());
+            }
+            txtMedico.ItemsSource = null;
+            txtMedico.Items.Clear();
+            txtMedico.ItemsSource = data;
+        }
+
+        private void btnAceptarAgPaciente_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtMedico.SelectedValue != null && txtDni.Text != "DNI")
+            {
+                //int idpaciente 
+            }else MessageBox.Show("Planilla Incompleta", "Error", MessageBoxButton.OK, MessageBoxImage.Warning); 
         }
     }
 }
