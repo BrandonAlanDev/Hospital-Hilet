85241b1895d3fd84d9f0f6c055decc3affa9a696
Luchi <lucianoceles@gmail.com>
2023-11-08 12:59:16 -0300

    Descargar Ingreso andando

--- MaquetaParaFinal/Clases/Conectar.cs
+++ MaquetaParaFinal/Clases/Conectar.cs
@@ -227,15 +227,18 @@ namespace MaquetaParaFinal.Clases
                     "p.Dni, " +
                     "CONCAT(pro.Nombre_Profesional,' ',pro.Apellido_Profesional) AS Medico, " +
                     "i.Fecha_Ingreso AS 'Fecha De Ingreso', " +
-                    "i.Retirado AS 'Fecha De Retiro' " +
-                    "COUNT(pxi.Fk_Id_Ingresos) AS Practicas" +
+                    "i.Retirado AS 'Fecha De Retiro', " +
+                    "+COUNT(pxi.Fk_Id_Ingresos) AS Practicas " +
                     "FROM Ingresos AS i " +
-                        "INNER JOIN Profesionales AS pro ON i.Fk_Id_Profesionales = pro.Pk_Id_Profesionales " +
-                        "INNER JOIN Pacientes AS p ON p.Pk_Id_Pacientes = i.Fk_Id_Paciente " +
-                        "INNER JOIN PracticasxIngresos AS pxi ON pxi.Fk_Id_Ingresos = i.Pk_Id_Ingresos" +
-                            "WHERE (p.Baja_Pacientes IS NULL) AND (pro.Baja_Profesional IS NULL) " +
-                    "GROUP BY ID, Paciente,Dni,'Fecha de Ingreso','Fecha de Retiro' " +
-                    "ORDER BY Paciente,Dni ,Medico,'Fecha De Ingreso', 'Fecha De Retiro';";
+                        "LEFT JOIN Profesionales AS pro ON i.Fk_Id_Profesionales = pro.Pk_Id_Profesionales " +
+                        "LEFT JOIN Pacientes AS p ON p.Pk_Id_Pacientes = i.Fk_Id_Paciente " +
+                        "LEFT JOIN PracticasxIngresos AS pxi ON pxi.Fk_Id_Ingresos = i.Pk_Id_Ingresos " +
+                    "WHERE p.Baja_Pacientes IS NULL AND pro.Baja_Profesional IS NULL " +
+                    "GROUP BY " +
+                        "i.Pk_Id_Ingresos, CONCAT(p.Nombre_Paciente,' ',p.Apellido_Paciente), p.Dni, " +
+                        "CONCAT(pro.Nombre_Profesional,' ',pro.Apellido_Profesional), i.Fecha_Ingreso, i.Retirado " +
+                    "ORDER BY " +
+                        "Paciente, Dni, Medico, 'Fecha De Ingreso', 'Fecha De Retiro';";
                 SqlDataAdapter command = new SqlDataAdapter(consulta, conexion);
                 DataTable tabla = new DataTable();
                 command.Fill(tabla);
@@ -340,24 +343,30 @@ namespace MaquetaParaFinal.Clases
             using (SqlConnection conexion = new SqlConnection(contrasenia))
             {
                 string consulta = $"SELECT i.Pk_Id_Ingresos AS ID, " +
-                $"p.Nombre_Paciente AS Paciente, " +
-                $"p.Apellido_Paciente AS Apellido, " +
-                $"p.Dni, " +
-                $"i.Fecha_Ingreso AS 'Fecha De Ingreso', " +
-                $"i.Retirado AS 'Fecha De Retiro', " +
-                $"CONCAT(pro.Nombre_Profesional, ' ', pro.Apellido_Profesional) AS Medico, " +
+                        $"CONCAT(p.Nombre_Paciente,' ',p.Apellido_Paciente) AS Paciente, " +
+                        $"p.Dni, " +
+                        $"CONCAT(pro.Nombre_Profesional,' ',pro.Apellido_Profesional) AS Medico, " +
+                        $"i.Fecha_Ingreso AS 'Fecha De Ingreso', " +
+                        $"i.Retirado AS 'Fecha De Retiro', " +
+                        $"COUNT(pxi.Fk_Id_Ingresos) AS Practicas " +
                     $"FROM Ingresos AS i " +
-                        $"INNER JOIN Profesionales AS pro ON i.Fk_Id_Profesionales = pro.Pk_Id_Profesionales " +
-                        $"INNER JOIN Pacientes AS p ON p.Pk_Id_Pacientes = i.Fk_Id_Paciente " +
-                        $"WHERE LOWER(p.Nombre_Paciente) LIKE '%{buscar}%' OR " +
-                            $"LOWER(p.Apellido_Paciente) LIKE '%{buscar}%' OR " +
-                            $"LOWER(p.Dni) LIKE '%{buscar}%' OR " +
-                            $"LOWER(i.Fecha_Ingreso) LIKE '%{buscar}%' OR " +
-                            $"LOWER(i.Retirado) LIKE '%{buscar}%' OR " +
-                            $"LOWER(pro.Nombre_Profesional) LIKE '%{buscar}%' OR " +
-                            $"LOWER(pro.Apellido_Profesional) LIKE '%{buscar}%' AND (p.Baja_Pacientes IS NULL) AND (pro.Baja_Profesional IS NULL) AND " +
-                            $"(i.Retirado IS NULL)" +
-                            $"ORDER BY Paciente, Apellido, 'Fecha De Ingreso', 'Fecha De Retiro', Medico, 'Apellido Medico'";
+                        $"LEFT JOIN Profesionales AS pro ON i.Fk_Id_Profesionales = pro.Pk_Id_Profesionales " +
+                        $"LEFT JOIN Pacientes AS p ON p.Pk_Id_Pacientes = i.Fk_Id_Paciente " +
+                        $"LEFT JOIN PracticasxIngresos AS pxi ON pxi.Fk_Id_Ingresos = i.Pk_Id_Ingresos " +
+                    $"WHERE " +
+                        $"LOWER(p.Nombre_Paciente) LIKE '%{buscar}%' OR " +
+                        $"LOWER(p.Apellido_Paciente) LIKE '%{buscar}%' OR " +
+                        $"LOWER(p.Dni) LIKE '%{buscar}%' OR " +
+                        $"LOWER(i.Fecha_Ingreso) LIKE '%{buscar}%' OR " +
+                        $"LOWER(i.Retirado) LIKE '%{buscar}%' OR " +
+                        $"LOWER(pro.Nombre_Profesional) LIKE '%{buscar}%' OR " +
+                        $"LOWER(pro.Apellido_Profesional) LIKE '%{buscar}%' " +
+                        $"AND (p.Baja_Pacientes IS NULL) AND (pro.Baja_Profesional IS NULL) AND (i.Retirado IS NULL) " +
+                    $"GROUP BY " +
+                        $"i.Pk_Id_Ingresos, CONCAT(p.Nombre_Paciente,' ',p.Apellido_Paciente), p.Dni, " +
+                        $"CONCAT(pro.Nombre_Profesional,' ',pro.Apellido_Profesional), i.Fecha_Ingreso, i.Retirado " +
+                    $"ORDER BY " +
+                        $"Paciente, Dni, Medico, 'Fecha De Ingreso', 'Fecha De Retiro';";
 
                 SqlDataAdapter command = new SqlDataAdapter(consulta, conexion);
                 DataTable tabla = new DataTable();
