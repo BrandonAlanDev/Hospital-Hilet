acef31630099f299db743c2788d0bfaefff89aac
BrandonAlanDev <brandoncarabajal@gmail.com>
2023-11-11 18:39:46 -0300

    Especialidad

--- MaquetaParaFinal/Clases/Especialidad.cs
+++ MaquetaParaFinal/Clases/Especialidad.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace MaquetaParaFinal.Clases
+namespace MaquetaParaFinal.View
 {
-    class Especialidad
+    public partial class Especialidad : Page
     {
+
+        private void EnterBuscar(object sender, KeyEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void ClickBuscar(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void DataGridEspecialidad_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void DataGridEspecialidad_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void DataGridEspecialidad_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void btAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void btModificar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void btEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
     }
 }
