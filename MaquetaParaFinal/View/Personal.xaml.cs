3c97f579b12e1104a504667b5bd25876149b64ae
BrandonAlanDev <brandoncarabajal@gmail.com>
2023-11-11 18:15:42 -0300

    MAs cambios

---
+++ MaquetaParaFinal/View/Personal.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaquetaParaFinal.View
+{
+    /// <summary>
+    /// Interaction logic for Personal.xaml
+    /// </summary>
+    public partial class Personal : Page
+    {
+        public Personal()
+        {
+            InitializeComponent();
+        }
+
+        private void btAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void btModificar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void btEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void EnterBuscar(object sender, KeyEventArgs e)
+        {
+            //TO-DO
+        }
+
+        private void ClickBuscar(object sender, RoutedEventArgs e)
+        {
+            //TO-DO
+        }
+    }
+}
