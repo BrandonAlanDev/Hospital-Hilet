7b332176f2247ccbba3d365930280c3c3e2d4bf6
BrandonAlanDev <brandoncarabajal@gmail.com>
2023-11-11 18:54:14 -0300

    Agregar Personal

---
+++ MaquetaParaFinal/View/Agregar/AgregarPersonal.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaquetaParaFinal.View.Agregar
+{
+    /// <summary>
+    /// Interaction logic for AgregarPersonal.xaml
+    /// </summary>
+    public partial class AgregarPersonal : Window
+    {
+        public AgregarPersonal()
+        {
+            InitializeComponent();
+        }
+
+        private void SoloNumero(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ControlarNombre(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAceptarAgPaciente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelarAgPaciente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAgregarEspecialidad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnAgregarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Principal_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+    }
+}
